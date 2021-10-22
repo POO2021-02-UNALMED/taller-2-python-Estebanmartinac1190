fc7156db53f19c17cd90c799ca8815e942c0c336
Estebanmartinac1190 <92158255+Estebanmartinac1190@users.noreply.github.com>
2021-10-22 04:39:19 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,18 +1,15 @@
 class Asiento:
     pass
-
     def __init__(self,color, precio , registro):
         self.color = color
         self.precio = precio
         self.registro = registro
-
     def cambiarColor(self, color):
         if color=='rojo' or color=='verde' or color=='amarillo' or color=='negro' or color=='blanco':
             self.color = color
 
 class Auto:
     pass
-
     def __init__(self, modelo, precio, asientos, marca, motor, registro):
         self.modelo = modelo
         self.precio = precio
@@ -39,19 +36,17 @@ class Auto:
                 return loco
         return loco
 
-
 class Motor:
-    pass
-    def __init__(self, numeroCilindros, registro, tipo):
+    def __init__(self, numeroCilindros, tipo, registro):
+        self.tipo = tipo
         self.numeroCilindros = numeroCilindros
         self.registro = registro
-        self.tipo = tipo
 
-    def cambiarRegistro(self,registro):
+    def cambiarRegistro(self, registro):
         self.registro=registro
 
     def asignarTipo(self, tipo):
-        if tipo =="electrico":
+        if tipo =="gasolina":
             self.tipo = tipo
-        elif tipo == "gasolina":
+        elif tipo == "electrico":
             self.tipo = tipo
